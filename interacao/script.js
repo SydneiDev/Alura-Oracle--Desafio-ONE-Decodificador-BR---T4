e4bb7d0a3f51fe01c686e31cf3f1876850286ac5
Sydnei Pereira <111784147+SydneiDev@users.noreply.github.com>
2023-01-09 17:05:06 -0300

    Update script.js

--- interacao/script.js
+++ interacao/script.js
@@ -6,7 +6,7 @@ function criptografar(){
 
   var texto = textInput.value;
 
-  var resultCripto = texto.replace(/a/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+  var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
   
   document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto + 
